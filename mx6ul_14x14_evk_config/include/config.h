a901eb8fd003920fe9428cf61ae9c865c4a849c1
leo.liu <liu090@sina.com>
2017-05-19 23:52:01 -0700

    metro u-boot base on 4.1.15.2 release

---
+++ mx6ul_14x14_evk_config/include/config.h
@@ -0,0 +1,8 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/freescale/mx6ul_14x14_evk/imximage.cfg
+#define CONFIG_BOARDDIR board/freescale/mx6ul_14x14_evk
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6ul_14x14_evk.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ mx6ul_14x14_evk_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "Mar 17 2017"
+#define U_BOOT_TIME "15:57:12"
+#define U_BOOT_TZ "+0800"
+#define U_BOOT_DMI_DATE "03/17/2017"
